ea3867ebc70fd3f2791ec7d9ce71ca1a5cc88dde
SImran Tyagi <111629975+siimrann@users.noreply.github.com>
2024-06-20 14:48:53 +0530

    complete

---
+++ index.js
@@ -0,0 +1,28 @@
+const SMTPServer = require("smtp-server").SMTPServer;
+
+const server = new SMTPServer({
+  allowInsecureAuth: true,
+  authOptional: true,
+  onConnect(session, cb) {
+    console.log(`onConnect`, session.id);
+    // cb(new Error("Cannot accept"))
+    cb();
+  },
+  onMailFrom(address, session, cb) {
+    console.log(`onMailFrom`, address.address, session.id);
+    cb();
+  },
+  onRcptTo(address, session, cb) {
+    console.log(`onRcptTo`, address.address, session.id);
+    cb();
+  },
+  onData(stream, session, cb) {
+    stream.on(
+      "data",
+      (data) => console.log(`onData ${data.toString()}`),
+      stream.on("end", cb)
+    );
+  },
+});
+
+server.listen(25, () => console.log("Server running on port 25"));
